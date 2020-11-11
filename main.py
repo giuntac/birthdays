62c6b3d6b8365a8eff3e8b2ce2104e32fb924db6
Rachele Povelato <rachelepovelato@Racheles-MBP.home>
2020-11-11 14:37:35 +0100

    Modify main to solve issue

--- main.py
+++ main.py
@@ -4,9 +4,4 @@ from birthdays import return_birthday
 
 return_birthday('Albert Einstein')
 return_birthday('Alan Turing')
-
-
-
-
-
-My birthday is on the 16th of January
+return_birthday('Barack Obama')
