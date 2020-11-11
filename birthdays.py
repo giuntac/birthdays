3dceed035b3165db86c2144315f70eda0a296325
giuliosmall <giulio.piccolo@me.com>
2020-11-11 17:34:54 +0100

    added MJ's birthday

--- birthdays.py
+++ birthdays.py
@@ -6,7 +6,8 @@ birthdays = {
     'Donald Trump': '06/14/1946',
     'Rowan Atkinson': '01/6/1955',
     'Giulio Piccolo': '05/10/1994',
-    'Cecilia Giunta':'11/02/1999'}
+    'Cecilia Giunta':'11/02/1999',
+    'Micheal Jordan': '17/02/1963'}
 
 def print_birthdays():
     print('Welcome to the birthday dictionary. We know the birthdays of these people:')
